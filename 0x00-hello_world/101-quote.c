087b8fdfaf65812a99888dc7b73d7e00ce874e76
Catherine <catherinekiplagat95@gmail.com>
2023-02-16 04:17:48 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
  * main -Program written without using printf or puts
  * Return: 1
  */
-int(void)main
+int main(void)
 {
 	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
