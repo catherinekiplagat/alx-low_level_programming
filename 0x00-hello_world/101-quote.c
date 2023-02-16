8f19fc18a4b23a53d6923aa140d141e8d38420b2
Catherine <catherinekiplagat95@gmail.com>
2023-02-16 05:31:28 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  * main -Program written without using printf or puts
@@ -6,6 +5,6 @@
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
