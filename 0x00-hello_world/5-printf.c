7d650e33fda15d9927d6f26e2fe88c912f4821e6
Catherine <catherinekiplagat95@gmail.com>
2023-02-16 00:45:15 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main -Program that prints using printf function followed by a new line
+* Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
