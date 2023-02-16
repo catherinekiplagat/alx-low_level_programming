49556b0835ef83a660ada44963566d42d3d718bb
Catherine <catherinekiplagat95@gmail.com>
2023-02-16 03:50:31 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <unstdio.h>
+#include <unistd.h>
 /**
  * main -Program written without using printf or puts
  * Return: 1
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n,stdout);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
