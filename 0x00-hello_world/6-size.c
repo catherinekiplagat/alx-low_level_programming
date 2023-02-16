90dc48f108b41f2a8f71dbc65798dbb62c52bd55
Catherine <catherinekiplagat95@gmail.com>
2023-02-16 01:58:37 -0800

    -

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,7 +16,6 @@ printf("size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
-break;
 printf("size64\n");
 printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
