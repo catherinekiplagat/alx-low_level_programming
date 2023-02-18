0bffeb484f79daa5f0175e78f7de7f7328e260d1
Catherine <catherinekiplagat95@gmail.com>
2023-02-18 00:29:15 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,10 +18,10 @@ int main(void)
 	/* your code goes there */
 	a = n % 10;
 	if (a > 5)
-		printf("last digit of %d is %d and is greater than 5\n", n, a);
+		printf("Last digit of %d is %d and is greater than 5\n", n, a);
 	else if (a == 0)
-		printf("last digit of  %d is %d and is zero\n", n, a);
+		printf("Last digit of  %d is %d and is zero\n", n, a);
 	else if (a < 6 && a != 0)
-		printf("last digit of %d is %d and is less than 6 and not 0\n", n, a);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main-program that prints the alphabet in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	c = ('a');
+
+	while (c <= ('z'))
+			{
+			putchar("*c\n");
+			c++;
+			}
+			return (0);
+}
