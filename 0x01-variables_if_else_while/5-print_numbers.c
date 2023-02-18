75d9637141043486bc4edcd29f3336eeaebb3263
Catherine <catherinekiplagat95@gmail.com>
2023-02-18 04:04:05 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main-program that prints all single digit numbers of base 10 starting from 0
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for
+	(a = 0;)
+		(a < 10;)
+	{
+		printf("a\n");
+		a++;
+	}
+	return (0);
+}
+
+
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,1 @@
-Print all the letters except q and e
+ program that prints the lowercase alphabet in reverse, followed by a new line
