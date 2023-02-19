b046bc035b85ad8cb47f375375fc6e0111534a8a
Catherine <catherinekiplagat95@gmail.com>
2023-02-19 09:05:23 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 /**
  * main-program that prints combinations of single-digit numbers
- *Return: 0
+ * Return: 0
  */
 int main(void)
 {
 	int i = '0';
 
-	for (i = '0'; i <= '9'; i++)
+	while (i < 10)
 	{
-		putchar(i);
+		putchar(i + '0');
 		putchar(',');
 		putchar(' ');
+		i++;
 	}
 	putchar('\n');
 	return (0);
