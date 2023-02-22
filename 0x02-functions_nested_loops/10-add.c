2506cb4d2207b46fa1be2db3b0a4e4339c02d7c0
Catherine <catherinekiplagat95@gmail.com>
2023-02-22 15:02:22 -0800

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers and returns the result.
+ * @a: integer one.
+ * @b: integer two.
+ * Return: Add of integer one and integer two.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
