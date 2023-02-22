3d0e09d4607657f7e159efb568db1edb86b34302
Catherine <catherinekiplagat95@gmail.com>
2023-02-22 10:16:56 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - alphabet in lowecase
+ * Return: 0 when successful
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+
+}
