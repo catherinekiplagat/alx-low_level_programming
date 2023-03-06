76b8bb5a51d6c26eb680cf334824abd2f56dc9b9
Catherine <catherinekiplagat95@gmail.com>
2023-03-06 13:24:18 -0800

    2-strchr.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,3 +3,4 @@ char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 void set_string(char **s, char *to);
 void print_diagsums(int *a, int size);
+char *_strchr(char *s, char c);
