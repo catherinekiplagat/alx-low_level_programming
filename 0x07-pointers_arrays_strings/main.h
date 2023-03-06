8f9a6f1c5c1736f6d81b6a6f9773e1717e9f147d
Catherine <catherinekiplagat95@gmail.com>
2023-03-06 13:16:29 -0800

    8-print_diagsums

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,3 +2,4 @@ int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 void set_string(char **s, char *to);
+void print_diagsums(int *a, int size);
