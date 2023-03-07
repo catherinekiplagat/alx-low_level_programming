3d45975340cadbc6e831c2adba9cc30a3b01cfde
Catherine <catherinekiplagat95@gmail.com>
2023-03-07 09:13:58 -0800

    3-strspn.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,3 +4,4 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 void set_string(char **s, char *to);
 void print_diagsums(int *a, int size);
 char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
