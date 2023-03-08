42f54a6deaee23a637ea496d60e4a6bac8cadafc
Catherine <catherinekiplagat95@gmail.com>
2023-03-08 12:39:03 -0800

    100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,15 @@
+#include  "main.h"
+#include <stdio.h>
+
+/**
+ * is_palindrome - detects if a string is a palindrome.
+ * @s: string.
+ * Return: 1 if s is a palindrome, 0 if not.
+ */
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+		return (1);
+	else
+			return (0);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,3 +1,4 @@
 int _putchar(char c);
 void _puts_recursion(char *s);
 int wildcmp(char *s1, char *s2);
+int is_palindrome(char *s);
